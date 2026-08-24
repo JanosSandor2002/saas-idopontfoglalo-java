aea9d47ccfa663412c5a9884df86cbef1e6bc98d
Jantsee <sandorj2002@gmail.com>
2026-08-24 16:57:27 +0200

    Initial commit: Spring Boot alapváz PostgreSQL kapcsolattal

---
+++ src/test/java/com/jantsee/idopontfoglalo/IdopontfoglaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jantsee.idopontfoglalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdopontfoglaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
